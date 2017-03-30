d62ac4bccdd6cc9524be41a41831eb5763a64111
santtone <anttonen.sami@gmail.com>
2017-03-30 22:18:34 +0300

    Added material 2 components and material design icons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
 
-import { AppComponent } from './app.component';
-import { ContactStoreService } from './contact/services/contact-store.service';
-import { ContactListComponent } from './contact/contact-list/contact-list.component'
-import { ContactListItemComponent } from './contact/contact-list/contact-list-item.component'
+import {AppComponent} from './app.component';
+import {ContactStoreService} from './contact/services/contact-store.service';
+import {ContactListComponent} from './contact/contact-list/contact-list.component';
+import {ContactListItemComponent} from './contact/contact-list/contact-list-item.component';
+import {MaterialRootModule} from '@angular/material';
 
 @NgModule({
   declarations: [
@@ -17,9 +18,11 @@ import { ContactListItemComponent } from './contact/contact-list/contact-list-it
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MaterialRootModule
   ],
   providers: [ContactStoreService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
