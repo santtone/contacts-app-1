34a37e11fe7cf6bad8be52f30d5ff07a5aa15946
santtone <sami.anttonen@trimble.com>
2017-04-19 00:30:27 +0300

    Refactoring sidenav UI

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,6 +1,7 @@
-import {Component, HostListener, NgZone, OnInit} from '@angular/core';
+import {Component, HostListener, OnInit, ViewChild} from '@angular/core';
 import {NavigationEnd, Router} from "@angular/router";
 import * as _ from 'lodash';
+import {MdSidenav} from "@angular/material";
 
 @Component({
   selector: 'ca-app-root',
@@ -12,18 +13,21 @@ export class AppComponent implements OnInit {
   toolbarDisabled: boolean;
   sidenavMode: string;
 
+  @ViewChild('sidenav') sidenav: MdSidenav;
+
   @HostListener('window:resize', ['$event'])
   onWindowResize(event) {
     let width = event ? event.target.innerWidth : window.innerWidth;
     this.sidenavMode = width >= 600 ? 'side' : 'over';
   }
 
-  constructor(private router: Router, private ngZone: NgZone) {
+  constructor(private router: Router) {
     this.toolbarDisabled = false;
     this.sidenavMode = 'over'
   }
 
   ngOnInit(): void {
+    console.log(this.sidenav);
     this.onWindowResize(null);
     this.router.events
       .subscribe(event => {
@@ -35,5 +39,9 @@ export class AppComponent implements OnInit {
           this.toolbarDisabled = false;
         }
       });
+  }
+
+  toggle(){
+    this.sidenav.toggle(!this.sidenav._isOpened);
   }
 }
