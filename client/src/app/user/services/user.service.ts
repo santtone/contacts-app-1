b7e6164018178c7edf4e86a4dd47a4322b91f9c9
santtone <sami.anttonen@trimble.com>
2017-05-02 00:06:44 +0300

    Skipping authentication on local storage build

--- client/src/app/user/services/user.service.ts
+++ client/src/app/user/services/user.service.ts
@@ -1,15 +1,22 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {AuthenticationService} from "./authentication.service";
 import {UserApiService} from "./user-api.service";
+import {environment} from "../../../environments/environment";
+import {Observable} from "rxjs/Observable";
 
 @Injectable()
 export class UserService {
 
-  constructor(private auth: AuthenticationService, private userApiService: UserApiService) { }
+  constructor(private auth: AuthenticationService, private userApiService: UserApiService) {
+  }
 
-  login(username: string, password: string){
-    return this.auth.authenticate(username, password).flatMap(() =>{
-      return this.userApiService.login();
-    });
+  login(username: string, password: string) {
+    if (environment.endpointUrl) {
+      return this.auth.authenticate(username, password).flatMap(() => {
+        return this.userApiService.login();
+      });
+    } else {
+      return Observable.of(null);
+    }
   }
 }
